0f14a047c896a1cd0414745b0c1d2c7d6ce69bbd
christina ba <139413435+christinaba24@users.noreply.github.com>
2024-11-18 01:40:37 -0800

    add svg icons, themes, fonts

---
+++ assets/theme.js
@@ -0,0 +1,26 @@
+export default {
+  colors: {
+    White: "#FFFFFF",
+    LightGray: "#F5F5F5",
+    PurpleLight: "#D9D9FF",
+    PurpleMedium: "#9C9CFF", // colored in icons, most backgrounds
+    PurpleDark: "#8D8DFF",
+
+    textBlack: "#000000",
+    textGray: "#67717A",
+
+    Yellow: "#F7CD61",
+  },
+  sizes: {
+    icons: 16,
+
+    largeTitle: 32,
+    title1: 26,
+    title2: 20,
+    title3: 18,
+    headline: 18,
+    body: 15,
+    callout: 13,
+    caption: 11,
+  },
+};
